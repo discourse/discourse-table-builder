4c8cfa9559038b768fcbf548a29e935b8eaae9ca
Keegan George <kgeorge13@gmail.com>
2022-07-04 14:58:50 -0700

    FEATURE: Ability to build a table in the composer
    - Adds a button to the composer tools which triggers a modal window for a table builder
    - Table builder contains fields to add table contents, as well as buttons to align contents, and add additional tables/rows

--- javascripts/discourse/api-initializers/table-builder.js
+++ javascripts/discourse/api-initializers/table-builder.js
@@ -1,3 +1,24 @@
 import { apiInitializer } from "discourse/lib/api";
+import { action } from "@ember/object";
+import showModal from "discourse/lib/show-modal";
 
-export default apiInitializer("0.11.1", (api) => {});
+export default apiInitializer("0.11.1", (api) => {
+  api.modifyClass("component:d-editor", {
+    pluginId: "discourse-table-builder",
+
+    @action
+    showTableBuilder(event) {
+      showModal("table-builder-modal").set("toolbarEvent", event);
+    },
+  });
+
+  api.onToolbarCreate((toolbar) => {
+    toolbar.addButton({
+      id: "table-builder",
+      group: "insertions",
+      icon: "table",
+      sendAction: (event) => toolbar.context.send("showTableBuilder", event),
+      title: themePrefix("discourse_table_builder.composer.button"),
+    });
+  });
+});
