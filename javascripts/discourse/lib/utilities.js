682db8d10394b2013d5ee00493d30e7a0e588922
Keegan George <kgeorge13@gmail.com>
2022-08-10 09:43:42 -0700

    DEV: Use alternate fork of `array-to-table`

--- javascripts/discourse/lib/utilities.js
+++ javascripts/discourse/lib/utilities.js
@@ -3,45 +3,41 @@
 /**
  * Generate markdown table from an array of objects
  *
- * @see {@link https://github.com/Nijikokun/array-to-table|GitHub}:
+ * @see {@link https://github.com/Ygilany/array-to-table|GitHub}:
  *
  * @param  {Array} array    Array of objects
  * @param  {Array} columns  Optional, table column names, otherwise taken from the keys of the first object
- * @param  {String} alignment Optional table alignment. Can be 'center' (default), 'left' or 'right'
  *
  * @return {String} Markdown table
  */
-export function arrayToTable(array, columns, alignment = "center") {
+export function arrayToTable(array, columns) {
   var table = "";
-  var separator = {
-    left: ":---",
-    right: "---:",
-    center: "---",
-  };
 
   // Generate column list
   var cols = columns ? columns.split(",") : Object.keys(array[0]);
 
   // Generate table headers
+  table += "|";
   table += cols.join(" | ");
-  table += "\r\n";
+  table += "|\r\n|";
 
   // Generate table header seperator
   table += cols
     .map(function () {
-      return separator[alignment] || separator.center;
+      return "---";
     })
     .join(" | ");
-  table += "\r\n";
+  table += "|\r\n";
 
   // Generate table body
   array.forEach(function (item) {
+    table += "|";
     table +=
       cols
         .map(function (key) {
           return String(item[key] || "");
         })
-        .join(" | ") + "\r\n";
+        .join(" | ") + "|\r\n";
   });
 
   // Return table
