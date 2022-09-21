b9d93cf5862944c5c4489a7a9e8f3b5bf3e7ac39
Keegan George <kgeorge13@gmail.com>
2022-09-21 10:00:05 -0700

    Revert "DEV: Ensure helper or post exist before decorations (#27)"
    This reverts commit 2896c69a727f18414afe2d4799cde5efbb0c1293.

--- javascripts/discourse/api-initializers/table-editor.js
+++ javascripts/discourse/api-initializers/table-editor.js
@@ -73,9 +73,6 @@ export default apiInitializer("0.11.1", (api) => {
 
   api.decorateCookedElement(
     (post, helper) => {
-      if (!helper || !post) {
-        return;
-      }
       const canEdit = helper.widget.attrs.canEdit;
 
       if (!canEdit) {
