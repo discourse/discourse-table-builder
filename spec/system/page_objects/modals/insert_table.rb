e542ebf166213699a86ada47dac913e399313e92
Keegan George <kgeorge13@gmail.com>
2023-11-08 13:51:40 -0800

    DEV: Add specs

--- spec/system/page_objects/modals/insert_table.rb
+++ spec/system/page_objects/modals/insert_table.rb
@@ -14,10 +14,32 @@ module PageObjects
         find("#{MODAL_SELECTOR} .d-modal-cancel").click
       end
 
-      def select_cell(row, col)
+      def click_edit_reason
+        find("#{MODAL_SELECTOR} .btn-edit-reason").click
+      end
+
+      def type_edit_reason(text)
+        find("#{MODAL_SELECTOR} .edit-reason input").send_keys(text)
+      end
+
+      def find_cell(row, col)
         find(
           "#{SPREADSHEET_TABLE_SELECTOR} tbody tr[data-y='#{row}'] td[data-x='#{col}']"
-        ).click
+        )
+      end
+
+      def select_cell(row, col)
+        find_cell(row, col).double_click
+      end
+
+      def type_in_cell(row, col, text)
+        select_cell(row, col)
+        cell = find_cell(row, col).find("textarea")
+        cell.send_keys(text, :return)
+      end
+
+      def has_content_in_cell?(row, col, content)
+        find_cell(row, col).text == content
       end
     end
   end
